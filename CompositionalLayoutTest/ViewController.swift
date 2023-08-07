b70c450b9a71a5e59cfff3e8d09bdc3c115dd69f
Daniil <DOrlov@medlinesoft.ru>
2023-08-07 13:09:16 +0300

    Initial Commit

---
+++ CompositionalLayoutTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompositionalLayoutTest
+//
+//  Created by Даниил Орлов on 07.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
